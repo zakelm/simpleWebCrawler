3de128a12fc09927fb57af4aef4c134fc22e9d10
zakelm <zakelm@users.noreply.github.com>
2017-10-12 15:36:02 +0200

    Update crawler.java

--- src/crawler/crawler.java
+++ src/crawler/crawler.java
@@ -19,17 +19,11 @@ import org.jsoup.select.Elements;
 public class crawler{
 	
 	public List<String> readFile( File fileName, String className) throws IOException{
-
-	    System.out.println(fileName);
 		ArrayList<String> links = new ArrayList<String>();
-		
+
 		List<String> fileContent = FileUtils.readLines(fileName);
-		
+
 		return fileContent;  
-	}
-	
-	public void dt() {
-		System.out.println("test");
 	}
 	
 	public static void main(String[] args) throws IOException, ConnectException{		
@@ -37,9 +31,8 @@ public class crawler{
 		String content;
 		FileWriter fw;
 		
-		crawler craw = new crawler();
-
-	    File folder      = new File( "links" );
+		crawler craw     = new crawler();
+		File folder      = new File( "links" );
 		File[] filesList = folder.listFiles();
 		String[] lines   = new String[ filesList.length ];
 
@@ -50,15 +43,12 @@ public class crawler{
 
 				for ( int j = 0; j < links.size(); j++ ) {
 				    try{
-				    	System.out.println((String)links.get(j));
-				    	document = Jsoup.connect((String)links.get(j)).get();	    
-					    content = document.select( ".job-paragraph" ).text();
-					    System.out.println(content);
-					    fw = new FileWriter( "data/" + file.getName() + "/" + j + ".txt" ); 
+				    	document = Jsoup.connect( ( String )links.get( j ) ).get();	    
+					content  = document.select( ".job-paragraph" ).text();
+					fw       = new FileWriter( "data/" + file.getName() + "/" + j + ".txt" ); 
 				        fw.write( content );      						 
 				        fw.close(); 
 				    }catch( MalformedURLException m) {
-				    	
 				    	System.err.print(m.getMessage() + j);
 				    } 
 				}
